48e5dab6911502626556e65149ac0339088014d3
Simon Escobar Benitez <s.escob.b.27@gmail.com>
2015-04-22 00:53:09 -0500

    Promises in Go

---
+++ promise_server.go
@@ -0,0 +1,33 @@
+// +build ignore
+
+package main
+
+import (
+	"encoding/binary"
+	"math/rand"
+	"net/http"
+)
+
+// START SERVER OMIT
+
+func main() {
+	odd := make([]int64, 100)
+	pair := make([]int64, 100)
+	for i := 0; i < 100; i++ {
+		pair[i] = int64(i)
+		odd[i] = int64(i + 1)
+		i++
+	}
+	rand.Seed(100)
+	http.HandleFunc("/odd", func(res http.ResponseWriter, req *http.Request) {
+		binary.Write(res, binary.LittleEndian, odd[rand.Int63n(100)])
+	})
+
+	http.HandleFunc("/pair", func(res http.ResponseWriter, req *http.Request) {
+		binary.Write(res, binary.LittleEndian, pair[rand.Int63n(100)])
+	})
+
+	http.ListenAndServe(":3000", nil)
+}
+
+// END SERVER OMIT
---
+++ promises.go
@@ -0,0 +1,67 @@
+// +build ignore
+
+package main
+
+import (
+	"encoding/binary"
+	"fmt"
+	"net/http"
+	"os"
+	"sync"
+)
+
+func panicIfError(err error) {
+	if err != nil {
+		fmt.Println("Fatal error ", err.Error())
+		os.Exit(1)
+	}
+}
+
+type Promise interface {
+	Then(func(value interface{}) Promise) Promise
+}
+
+type AsyncRequest struct {
+	Promise
+	body int64
+}
+
+func (aReq *AsyncRequest) Then(chain func(value interface{}) Promise) Promise {
+	return chain(aReq.body)
+}
+
+func (aReq *AsyncRequest) Get(path string) Promise {
+	wait := make(chan int64)
+	go func() {
+		res, err := http.Get(path)
+		defer res.Body.Close()
+		panicIfError(err)
+		var answer int64
+		err = binary.Read(res.Body, binary.LittleEndian, &answer)
+		panicIfError(err)
+		wait <- answer
+	}()
+	aReq.body = <-wait
+	return aReq
+}
+
+func main() {
+	req := &AsyncRequest{}
+	var wg sync.WaitGroup
+	for i := 1; i <= 100; i++ {
+		wg.Add(1)
+		go func(r *AsyncRequest, iter int) {
+			fmt.Println("Asynchronous Gets")
+			r.Get("http://localhost:3000/odd").Then(func(response interface{}) Promise {
+				fmt.Fprintf(os.Stdout, "(ODD) From Promise %d value => %v\n", iter, response)
+				req2 := &AsyncRequest{}
+				return req2.Get("http://localhost:3000/pair").Then(func(response interface{}) Promise {
+					fmt.Fprintf(os.Stdout, "(PAIR) From Innert Promise %d value => %v\n", iter, response)
+					wg.Done()
+					return nil
+				})
+			})
+		}(req, i)
+	}
+	wg.Wait()
+}
